521e88f1258a42d9dd0f318803c0dbda6d911c20
Roout <following2future@gmail.com>
2021-12-08 15:24:53 +0300

    solved: day8 part2

--- src/day8.py
+++ src/day8.py
@@ -22,10 +22,70 @@ class Part_1:
 
 class Part_2:
     def __init__(self) -> None:
-        # how many times digits 1,7,4,8 appear
+        # sum of the output
         self.result = 0
-    def parse_line(self, left: str, right: str) -> None:
-        pass
+    def parse_line(self, codes: str, output: str) -> None:
+        patterns = {}
+        # parse input codes
+        for s in codes.split():
+            size = len(s)
+            if size not in patterns:
+                patterns[size] = [set(s)]
+            else:
+                patterns[size] += [set(s)]
+
+        # confirm that all digits are in the input
+        assert(len(patterns) == len(digit_by_len))
+
+        digits = {
+            1: patterns[2][0].copy(),
+            7: patterns[3][0].copy(),
+            8: patterns[7][0].copy(),
+            4: patterns[4][0].copy()
+        }
+
+        # look for digit 9
+        for code in patterns[6]:
+            res = (digits[4] | digits[7]) ^ code
+            if len(res) == 1:
+                digits[9] = code.copy()
+                break
+        # look for mid
+        patterns[6].remove(digits[9]) 
+        assert(len(patterns[6]) == 2)
+
+        # find digits 0 and 6:
+        if len(patterns[6][0] & digits[1]) == 2:
+            digits[0] = patterns[6][0].copy()
+            digits[6] = patterns[6][1].copy()
+        else:
+            digits[0] = patterns[6][1].copy()
+            digits[6] = patterns[6][0].copy()
+        # 0 1 _ _ 4 _ 6 7 8 9
+        
+        # look for top-right
+        assert(len(digits[8] ^ digits[6]) == 1)
+        top_right = (digits[8] ^ digits[6]).pop()
+
+        digits[5] = digits[9] ^ { top_right }
+        assert(len(digits[5]) == 5)
+        patterns[5].remove(digits[5]) 
+        # 0 1 _ _ 4 5 6 7 8 9
+        if len(patterns[5][0] & digits[1]) == 2:
+            digits[3] = patterns[5][0].copy()
+            digits[2] = patterns[5][1].copy()
+        else:
+            digits[3] = patterns[5][1].copy()
+            digits[2] = patterns[5][0].copy()
+        
+        n = ''
+        for value in output.rstrip('\n').split():
+            s = set(value)
+            for i in range(10):
+                if digits[i] == s:
+                    n += str(i)
+                    break
+        self.result += int(n)
 
 part_1 = Part_1()
 part_2 = Part_2()
