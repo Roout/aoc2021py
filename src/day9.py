e494e7b67e2ac084c14b23a3e14836513dfc39c7
Roout <following2future@gmail.com>
2021-12-09 12:10:58 +0300

    solved: day9 parts 1 and 2

---
+++ src/day9.py
@@ -0,0 +1,75 @@
+
+dx = [-1, 1, 0,  0]
+dy = [ 0, 0, 1, -1]
+
+def in_bounds(map, x, y):
+    height = len(map)
+    assert(height > 0)
+    width = len(map[0])
+    return 0 <= x < width and 0 <= y < height
+
+def part_1(map):
+    s = 0
+    width = len(map[0])
+    height = len(map)
+    for i in range(height):
+        for j in range(width):
+            is_lowest = True
+            for k in range(4):
+                x = dx[k] + j
+                y = dy[k] + i
+                if in_bounds(map, x, y) and map[i][j] >= map[y][x]:
+                    is_lowest = False
+                    break
+            if is_lowest:
+                s += map[i][j] + 1
+    return s
+
+def Bfs(map, start:tuple):
+    WALL = 9
+
+    height = len(map)
+    width = len(map[0])
+    
+    basin_size = 0
+    used = [[False] * width for row in range(height)]
+    que = [start]
+    used[start[1]][start[0]] = True
+    while len(que) > 0:
+        x, y = que.pop(0)
+        basin_size += 1
+        for k in range(4):
+            next_x = dx[k] + x
+            next_y = dy[k] + y
+            if in_bounds(map, next_x, next_y) and used[next_y][next_x] == False:
+                if map[next_y][next_x] != WALL and map[next_y][next_x] > map[y][x]:
+                    que.append((next_x, next_y))
+                    used[next_y][next_x] = True
+    return basin_size                
+
+def part_2(map):
+    basin_sizes = []
+    # solve
+    width = len(map[0])
+    height = len(map)
+    for i in range(height):
+        for j in range(width):
+            is_lowest = True
+            for k in range(4):
+                x = dx[k] + j
+                y = dy[k] + i
+                if in_bounds(map, x, y) and map[i][j] >= map[y][x]:
+                    is_lowest = False
+                    break
+            if is_lowest:
+                basin_sizes.append(Bfs(map, start = (j, i)))
+
+    basin_sizes.sort(reverse = True)
+    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+
+
+with open('../input/day9.txt') as istream:
+    map = [[int(n) for n in line.rstrip('\n')] for line in istream.readlines()]
+    print('Part 1: ', part_1(map.copy()))    
+    print('Part 2: ', part_2(map.copy()))    
+
