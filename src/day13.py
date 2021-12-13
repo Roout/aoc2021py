5daa000f56c74c5ae0ebfba017ac952ea4afca68
Roout <following2future@gmail.com>
2021-12-13 18:51:19 +0300

    complete day13 part 1

--- src/day13.py
+++ src/day13.py
@@ -1,18 +1,61 @@
 
+class Rect:
+    def __init__(self, x, y, width, height) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def contains(self, x, y) -> bool:
+        return self.x <= x < self.width and self.y <= y < self.height
+
 def dump_2d(arr2d:list):
     for row in arr2d:
-        print(row)
+        print(row.join())
     return
 
-SIZE = 15
-page = [['.'] * SIZE for x in range(SIZE)]
+page_size = [0, 0]
+points = set()
 fold_queue = []
+
+def fold_page(page_size: list, points: set, fold: tuple):
+    x = 0
+    y = 0
+    w = page_size[0]
+    h = page_size[1]
+    
+    if fold[0] == 'x':
+        x = fold[1] + 1
+    else:
+        y = fold[1] + 1
+    
+    transformed = set()
+    rect = Rect(x, y, w, h)
+    for (next_x, next_y) in points:
+        if (rect.contains(next_x, next_y)):
+            # transformed
+            if fold[0] == 'x':
+                # x = 2; w = 5
+                # (4, 0)
+                # 0 1 [2] 3 4 
+                transformed.add((fold[1] - (next_x - fold[1]), next_y))
+                page_size[0] = fold[1]
+            else:
+                transformed.add((next_x, fold[1] - (next_y - fold[1])))
+                page_size[1] = fold[1]
+        else:
+            transformed.add((next_x, next_y))
+    return transformed
+
+def part_1(page_size: list, points: set):
+    front_fold = fold_queue.pop(0)
+    points = fold_page(page_size, points, front_fold)
+    return len(points)
 
 with open('../input/day13.txt') as istream:
     for line in istream:
         line = line.rstrip('\n')
         if len(line) == 0:
-            print("Empty line")
             continue
         if line.find(',') < 0:
             fold = line.split(' ')[2]
@@ -20,8 +63,8 @@ with open('../input/day13.txt') as istream:
             fold_queue.append((axis, int(value)))
         else:
             x, y = [int(v) for v in line.split(',')]
-            page[y][x] = '#'
+            points.add((x, y))
+            if page_size[0] < x + 1: page_size[0] = x + 1
+            if page_size[1] < y + 1: page_size[1] = y + 1
 
-dump_2d(page)
-print(fold_queue)
-
+print("Part_1: ", part_1(page_size, points.copy()))
