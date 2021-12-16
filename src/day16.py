1a101076c93ffa899f74d6f4962ab5c1dc62df48
Roout <following2future@gmail.com>
2021-12-16 20:27:01 +0300

    solved day16 part 2

--- src/day16.py
+++ src/day16.py
@@ -1,6 +1,5 @@
 from functools import reduce
 import time
-from typing import Generator
 
 class Extractor:
     binary: str
@@ -15,76 +14,100 @@ class Extractor:
         self.start += count
         return ret
 
-def handle_literal(generator: Extractor):
-    shift = 5
-    stop = lambda bits: bits[0] == '0'
-    update = lambda init, next: init + next[1:]
+    def consume(self, count: int):
+        self.start += count
 
-    bit_used = 6 # packet's version (3) and packet's ID (3)
-    group = generator.next(shift)
+def handle_literal(generator: Extractor):
+    SHIFT = 5
+
+    group = generator.next(SHIFT)
     init = group[1:]
-    bit_used += 5
-    while not stop(group):
-        group = generator.next(shift)
-        init = update(init, group)
-        bit_used += 5
-    # consume padding 
-    # padding = (4 - (bit_used % 4)) % 4
-    # generator.next(padding)
+    while group[0] != '0':
+        group = generator.next(SHIFT)
+        init += group[1:]
     return init
 
-def handle_packet(generator: Extractor, depth: int):
+def handle_packet(generator: Extractor):
     LITERAL = 4
-    # PREFIX = '  ' * depth
 
     packet_version = int(generator.next(3), 2)
     packet_type_id = int(generator.next(3), 2)
 
-    # print(PREFIX, '{')
-    # print(PREFIX, "  Version", packet_version)
-    # print(PREFIX, "  Type id", packet_type_id)
-
     sum = packet_version
     if packet_type_id == LITERAL:
-        # print(PREFIX, "  Literal")
-        # skip literal
         handle_literal(generator)
     else:
         length_type_id = int(generator.next(1))
         if length_type_id == 1:
             # number of subpackets
             subpacket_count = int(generator.next(11), 2)
-            # print(PREFIX, "  Operator {} subpackets".format(subpacket_count))
             while subpacket_count > 0:
-                # assume it's only literals!!!
-                sum += handle_packet(generator, depth + 1)
+                sum += handle_packet(generator)
                 subpacket_count -= 1
         else:
             # number of bits contains in all subpackets
             subbin = generator.next(15)
             total_length = int(subbin, 2)
-            # print(PREFIX, "  Operator {} total length".format(total_length))
             while total_length > 0:
                 start = generator.start
-                sum += handle_packet(generator, depth + 1)
+                sum += handle_packet(generator)
                 total_length -= generator.start - start
-    # print(PREFIX, '}')
     return sum
 
+func_by_id = {
+    0: lambda init, literal: init + literal, 
+    1: lambda init, literal: init * literal, 
+    2: lambda init, literal: init if init < literal else literal, 
+    3: lambda init, literal: init if init > literal else literal, 
+    5: lambda init, literal: 1 if init > literal else 0, 
+    6: lambda init, literal: 1 if init < literal else 0, 
+    7: lambda init, literal: 1 if init == literal else 0
+}
+
+def calc_packet_value(generator: Extractor):
+    LITERAL = 4
+
+    generator.consume(3) # packet_version
+    packet_type_id = int(generator.next(3), 2)
+
+    result = None
+    if packet_type_id == LITERAL:
+        result = int(handle_literal(generator), 2)
+    else:
+        fn = func_by_id[packet_type_id]
+        length_type_id = int(generator.next(1))
+        if length_type_id == 1:
+            # number of subpackets
+            subpacket_count = int(generator.next(11), 2)
+            # init result
+            result = calc_packet_value(generator)
+            while subpacket_count > 1:
+                result = fn(result, calc_packet_value(generator))
+                subpacket_count -= 1
+        else:
+            # number of bits contains in all subpackets
+            subbin = generator.next(15)
+            total_length = int(subbin, 2)
+            # init result
+            start = generator.start
+            result = calc_packet_value(generator)
+            total_length -= generator.start - start
+            while total_length > 0:
+                start = generator.start
+                result = fn(result, calc_packet_value(generator))
+                total_length -= generator.start - start
+    return result
+
 def part_1(generator: Extractor):
-    return handle_packet(generator, 0)
+    return handle_packet(generator)
 
 def part_2(generator: Extractor):
-    pass
+    return calc_packet_value(generator)
 
 binary = str()
 with open("../input/day16.txt") as istream:
     accumulator = lambda init, token: init + bin(int(token, 16))[2:].zfill(4)
     binary = reduce(accumulator, istream.readline().rstrip('\n'), binary)
-    # print(binary)
-
-# {011 000 1 00000000010 {<1st> 000 000 0 000000000010110 { 000 100 01010 } { 101 100 01011 } } {<2nd> 001 000 1 00000000010 { 000 100 01100 } { 011 100 01101 00}}}
-
 
 begin = time.time()
 print('Part_1: {}, takes {}s'.format(part_1(Extractor(binary)), time.time() - begin))
